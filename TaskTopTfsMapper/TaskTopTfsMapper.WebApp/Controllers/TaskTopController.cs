ca63f93af0bc0e5ea61446a6b64cc5e459a806fd
patrick huber <prhuber@gmail.com>
2016-05-12 18:03:25 -0400

    initial revision

---
+++ TaskTopTfsMapper/TaskTopTfsMapper.WebApp/Controllers/TaskTopController.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+
+namespace TaskTopTfsMapper.WebApp.Controllers
+{
+    [RoutePrefix("")]
+    public class TaskTopController : ApiController
+    {
+        [HttpPost]
+        [Route("")]
+        public async Task<HttpResponseMessage> Post()
+        {
+            var jsonString = await Request.Content.ReadAsStringAsync();
+            var token = JObject.Parse(jsonString);
+            var uri = "http://louwebwts388:8080/api/v1/artifacts/";
+
+            var stringContent = new StringContent(
+                @"{""subscriptionid"" : ""String""}");
+            var httpClient = new HttpClient();
+            var response = await httpClient.PostAsync(uri, stringContent);
+
+            return new HttpResponseMessage(HttpStatusCode.Created);
+        }
+
+        [HttpGet]
+        [Route("")]
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
